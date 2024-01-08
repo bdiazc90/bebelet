17aa1ed169df203e5d4383c374af95f0d22d63d4
bdiazc90 <bdiazc90@gmail.com>
2024-01-07 21:16:26 -0500

    fix padding for mobile

--- app/numbers/[number]/page.tsx
+++ app/numbers/[number]/page.tsx
@@ -3,7 +3,7 @@ import FooterBar from "@/components/FooterBar";
 
 export default function PageNumber({ params }: { params: { number: string } }) {
 	return (
-		<main className="h-screen flex flex-col items-center justify-between py-[8vh] lg:py-[6vh]">
+		<main className="h-screen flex flex-col items-center justify-between pt-[6vh] pb-[12vh]">
 			<h2 className="text-xl lg:text-3xl text-center font-worksans">Bebe Let</h2>
 			<Number number={params.number} />
 			<FooterBar />
