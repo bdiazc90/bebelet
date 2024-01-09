6d267f9bb9255fbb2ff28074ef0fcea3f4cd48a6
bdiazc90 <bdiazc90@gmail.com>
2024-01-09 14:15:49 -0500

    fix localStorage

--- app/letters/page.tsx
+++ app/letters/page.tsx
@@ -1,18 +1,21 @@
-'use client';
+"use client";
 
-import { redirect } from 'next/navigation';
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
-import { all_letters as defaultLetters } from '@/lib/data';
-import { randomIntFromInterval } from '@/lib/utils';
-
-const lsLetters = localStorage.getItem("bbl.letters");
-const currentLetters = lsLetters ? JSON.parse(lsLetters) : defaultLetters;
-localStorage.setItem("bbl.letters", JSON.stringify(currentLetters));
-
-const max_qty = currentLetters.length - 1;
+import { all_letters as defaultLetters } from "@/lib/data";
+import { randomIntFromInterval } from "@/lib/utils";
 
 export default function PageLetters() {
-    const randomIndex = randomIntFromInterval(0, max_qty);
-    const newLetter = currentLetters.at(randomIndex)?.toLowerCase();
-    redirect(`/letters/${newLetter}`);
+	useEffect(() => {
+		if (window !== undefined && window.localStorage) {
+			const lsLetters = localStorage.getItem("bbl.letters");
+			const currentLetters = lsLetters ? JSON.parse(lsLetters) : defaultLetters;
+			localStorage.setItem("bbl.letters", JSON.stringify(currentLetters));
+			const max_qty = currentLetters.length - 1;
+			const randomIndex = randomIntFromInterval(0, max_qty);
+			const newLetter = currentLetters.at(randomIndex)?.toLowerCase();
+			redirect(`/letters/${newLetter}`);
+		}
+	}, []);
 }
--- app/numbers/page.tsx
+++ app/numbers/page.tsx
@@ -1,18 +1,21 @@
-'use client';
+"use client";
 
+import { useEffect } from "react";
 import { redirect } from "next/navigation";
 
 import { all_numbers as defaultNumbers } from "@/lib/data";
 import { randomIntFromInterval } from "@/lib/utils";
 
-const lsNumbers = localStorage.getItem("bbl.numbers");
-const currentNumbers = lsNumbers ? JSON.parse(lsNumbers) : defaultNumbers;
-localStorage.setItem("bbl.numbers", JSON.stringify(currentNumbers));
-
-const max_qty = currentNumbers.length - 1;
-
 export default function PageNumbers() {
-	const randomIndex = randomIntFromInterval(0, max_qty);
-	const newNumber = currentNumbers.at(randomIndex);
-	redirect(`/numbers/${newNumber}`);
+	useEffect(() => {
+		if (window !== undefined && window.localStorage) {
+			const lsNumbers = localStorage.getItem("bbl.numbers");
+			const currentNumbers = lsNumbers ? JSON.parse(lsNumbers) : defaultNumbers;
+			localStorage.setItem("bbl.numbers", JSON.stringify(currentNumbers));
+			const max_qty = currentNumbers.length - 1;
+			const randomIndex = randomIntFromInterval(0, max_qty);
+			const newNumber = currentNumbers.at(randomIndex);
+			redirect(`/numbers/${newNumber}`);
+		}
+	}, []);
 }
